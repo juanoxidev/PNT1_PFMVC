e3f9881e4900a06f4eb206a8f45dfedea1ff7bec
marvangonzalez <82179028+marvangonzalez@users.noreply.github.com>
2023-07-03 19:46:19 -0300

    display en envio

--- DeliveryApp/Models/Envio.cs
+++ DeliveryApp/Models/Envio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace DeliveryApp.Models
 {
     public enum Envio
     {
+        [Display(Name = "Retiro en local")]
         RETIRO_EN_LOCAL,
+        [Display(Name = "Envio a domicilio")]
         ENVIO_A_DOMICILIO,
+        [Display(Name = "Consumo en local")]
         CONSUMO_EN_LOCAL,
     }
 }
