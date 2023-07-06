5ef8f1e686195ffb676b6bfcd01ce272fa6ff77b
julizielo <julizielony@gmail.com>
2023-07-05 21:26:32 -0300

    intentando resolver un mensaje de error

--- DeliveryApp/Models/Pedido.cs
+++ DeliveryApp/Models/Pedido.cs
@@ -14,7 +14,7 @@ namespace DeliveryApp.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // que se autoincremente el id
         public int IdPedido { get; set; }
         [Required(ErrorMessage = "Debe seleccionar una fecha")]
-        [DataType(DataType.DateTime)]
+        [DataType(DataType.DateTime, ErrorMessage = "Debe seleccionar una fecha")]
         [FechaActual(ErrorMessage = "La fecha del pedido debe ser igual o posterior a la fecha actual.")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)]
         [Display(Name = "Fecha del pedido")]
@@ -33,7 +33,7 @@ namespace DeliveryApp.Models
         public int IdCliente { get; set; }
         [Required(ErrorMessage = "Debe ingresar un precio para el pedido")]
         [DataType(DataType.Currency, ErrorMessage ="Debe ingresar un precio para el pedido")]
-        [Range(1, 9999999)]
+        [Range(1, 9999999 , ErrorMessage="El precio debe encontrarse dentro del rango $1 - $9999999")]
         public decimal Precio { get; set; }
         [Required(ErrorMessage = "Debe seleccionar una forma de envio" , AllowEmptyStrings = true)]
         //[Required(AllowEmptyStrings = true)]
